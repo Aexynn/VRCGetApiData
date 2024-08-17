911ffa4c0c7e95ffbad47adfdef6f6070e58f436
CodeKyana <169302941+Kyuddle@users.noreply.github.com>
2024-08-17 08:43:22 +0200

    Update Concurrently line & Start name

--- launch.js
+++ launch.js
@@ -1,7 +1,7 @@
 const { spawn } = require("child_process");
 
 const command = "npm";
-const args = ["run", "all:launch"];
+const args = ["run", "start"];
 
 const process = spawn(command, args, { stdio: "inherit", shell: true });
 
